4a6b4de3de7b18ef7450c93670e8d911cc53034d
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2024-10-23 20:21:37 -0300

    [feat]: header edited

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,22 @@
+import { FileText } from "lucide-react";
+import Image from "next/image";
+
 export default function CustomerHeader() {
     return (
-        <header className="p-8">
-            <div>Sua logo aqui</div>
+        <header className="px-8 pt-1 flex justify-between items-center">
+
+            <Image src={"/logoExemplo.png"} alt="Logo de exemplo da sua empresa" width={96} height={96}></Image>
+            <button className="relative">
+                <FileText className="" size={32}></FileText>
+                <div className="absolute -bottom-3 -left-3 w-6 h-6  
+                    border border-gray-100 bg-white shadow'
+                    flex justify-center items-center rounded-full
+                ">
+
+                    <p className="text-center text-sm">1</p>
+                </div>
+
+            </button>
         </header>
     )
 }
